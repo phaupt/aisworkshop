606d0fd5a289c13210c9f0c4019e74067ff9236f
phaupt <philipp.haupt@swisscom.com>
2014-07-16 20:48:31 +0200

    Added PDF attributes

--- wks-04.sh
+++ wks-04.sh
@@ -8,4 +8,4 @@
 [ -f "$2" ] && rm -f $2
 
 # Call iText Java application
-java -cp ".:./itext/lib/*:./itext/jar/*" com.swisscom.ais.itext.SignPDF -d -config=./itext/signpdf.properties -type=sign -infile=$1 -outfile=$2
+java -cp ".:./itext/lib/*:./itext/jar/*" com.swisscom.ais.itext.SignPDF -d -config=./itext/signpdf.properties -type=sign -infile=$1 -outfile=$2 -reason="AIS Workshop Test" -location="Zürich"
